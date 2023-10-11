a19546f29476909be9427ea6f4bbc6d2f9e84041
LordNedStark <osisiogubenjamin1@gmail.com>
2023-10-11 12:17:16 +0100

    resolved expo login for teammate

--- pages/cart/Items.jsx
+++ pages/cart/Items.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { View, Text, Image, TouchableOpacity} from 'react-native';
 
 import addCartStyles from './addCartStyles.js';
@@ -6,6 +6,8 @@ import { ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome'; 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import ItemButton from '../const/ItemButton.js'
+import axios from 'axios';
+import BASE_URL from '../../secrets/.SecretConstants.js';
 
 
 const products = "products"
@@ -34,6 +36,8 @@ const items = [
 const Items = ({navigation}) => {
   const [total, setTotal] = useState(0.0);
   const [valuesFromStorage, setValuesFromStorage] = useState(0.0);
+  const [cartProducts, setCartProducts] = useState([])
+  const [isloading, setLoading] = useState(true)
 
   const increment = (item) => {
     // Implement your logic to increment item quantity here
@@ -42,6 +46,48 @@ const Items = ({navigation}) => {
   const decrement = (item) => {
     // Implement your logic to decrement item quantity here
   };
+
+  const fetchData =useCallback(async ()=>{
+    let value = await AsyncStorage.getItem("uniqueCartId")
+    let data = JSON.stringify(value)
+    // data = "data"
+
+  try {
+    const response = await axios.get(
+      BASE_URL+"/api/v1/cartProduct/findCartByUniqueCartId/"+ data, 
+    );
+    console.log(response.data);
+    if (response.status !== 200){
+      throw new Error("Product not found")
+    }else if (response.status === 200) {
+      
+
+        setCartProducts(response.data.data)
+      
+        setLoading(false)
+
+    console.log(response.data.data);
+    }
+    
+    
+  } catch (error) {
+    if(error.message === "Request failed with status code 500" ){
+      
+      console.log(error.message)
+    }else if(error.message === "Product not found") {
+      console.log(error.message);
+    }else{
+      console.log(error.message);
+    }
+
+      
+    
+  }
+}, []);
+
+useEffect(() => {
+fetchData();
+}, [fetchData]);
 
   const storageRetrival = async ()=>{
     let productsArray = await AsyncStorage.getItem(products);
